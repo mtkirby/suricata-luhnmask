a06500dc18aee78053b5580206243a0ba6493330
mtkirby <mtkirby@users.noreply.github.com>
2020-02-22 16:23:52 -0600

    Add files via upload

--- luhnmask-run.sh
+++ luhnmask-run.sh
@@ -4,17 +4,18 @@
 if egrep -q "eve.json$" /etc/suricata/*.yaml \
 || ! pgrep -f luhnmask.pl >/dev/null 2>&1
 then
-    systemctl stop suricata
+    logger "$_ restarting processes for suricata and luhnmask.pl" 
+    systemctl stop suricata 
     sleep 5
-    rm -f /var/run/suricata.pid >/dev/null 2>&1
-    pkill luhnmask.pl >/dev/null 2>&1
+    rm -f /var/run/suricata.pid 
+    pkill luhnmask.pl 
     perl -pi -e 's/eve.json$/eve.json.fifo/g' /etc/suricata/*.yaml
-    rm -f /var/log/suricata/eve.json >/dev/null 2>&1
-    mkfifo /var/log/suricata/eve.json.fifo >/dev/null 2>&1
-    chown logstash:logstash /var/log/suricata/eve.json.fifo >/dev/null 2>&1
-    touch /var/log/suricata/eve.json >/dev/null 2>&1
-    chown logstash:logstash /var/log/suricata/eve.json >/dev/null 2>&1
-    nohup /root/luhnmask.pl >/dev/null 2>&1 &
+    rm -f /var/log/suricata/eve.json 
+    mkfifo /var/log/suricata/eve.json.fifo 
+    chown logstash:logstash /var/log/suricata/eve.json.fifo 
+    touch /var/log/suricata/eve.json 
+    chown logstash:logstash /var/log/suricata/eve.json 
+    nohup /root/luhnmask.pl 
     disown
     systemctl restart suricata
 fi
